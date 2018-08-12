459cce1f2a49c30f62ce23be098d036c1491dbd7
ChenghanKe <31741393+ChenghanKe@users.noreply.github.com>
2018-08-11 19:24:17 -0700

    my first push

---
+++ My Portfolio/indexJavascript.js
@@ -0,0 +1,22 @@
+//jquery
+
+// $(document).ready(function(){
+      
+//       $("#overlayNavigation").hide();
+
+// 	  $("#hamber").click(function(){
+
+// 	  	$("#overlayNavigation").toggle(200);
+// 	  })
+
+// })
+
+
+$(document).ready(function(){
+      
+	  $("#hamber").click(function(){
+
+	  	$("#overlayNavigation").slideToggle(740);
+	  })
+
+})
